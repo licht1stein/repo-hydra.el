8513ff8dd867ca05a77622980346a04ac1202d50
licht1stein <mb@m1k.pw>
2023-05-06 10:44:04 +0100

    First commit

---
+++ repo-hydra.el
@@ -0,0 +1,39 @@
+;;; repo-hydra.el --- git repo specific hydras -*- lexical-binding: t; -*-
+;;
+;; Copyright (c) 2023, Mykhaylo Bilyanskyy <mb@m1k.pw>
+;;
+;; Author: Mykhaylo Bilyanskyy <mb@m1k.pw>
+;; Maintainer: Mykhaylo Bilyanskyy <mb@m1k.pw>
+;; Version: 0.1
+;; Package-Requires: ((hydra "0.15.0"))
+;;
+;; Created: 06 May 2023
+;;
+;; URL: https://github.com/licht1stein/repo-hydra.el
+;;
+;; License: GPLv3
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see
+;; <http://www.gnu.org/licenses/>.
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+
+
+(provide 'repo-hydra)
+;;; repo-hydra.el ends here
